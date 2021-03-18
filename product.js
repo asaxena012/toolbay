e2350a273dbdc0f0271688fdc164546c692552fe
Aditya Saxena <60032144+asaxena012@users.noreply.github.com>
2021-03-18 16:27:06 +0530

    fix: cards undefined bug fixed, feature: display all products feature added

--- product.js
+++ product.js
@@ -1,47 +1,69 @@
 window.onload = function () {
-    const prod = ["2-channel-relay-module", "bluetooth-module", "breadboard-capacitor-resistor-led-switch", "conductive-thread", "esc", "finger-print-scanner",
-        "gprs-module", "gryo", "ldr", "lidar", "multimeter", "oscilloscope", "pulse-sensor-amped", "solar-charge-controller", "soldering-kit", "wifi-module"];
+  const prod = [
+    "2-channel-relay-module",
+    "bluetooth-module",
+    "breadboard-capacitor-resistor-led-switch",
+    "conductive-thread",
+    "esc",
+    "finger-print-scanner",
+    "gprs-module",
+    "gryo",
+    "ldr",
+    "lidar",
+    "multimeter",
+    "oscilloscope",
+    "pulse-sensor-amped",
+    "solar-charge-controller",
+    "soldering-kit",
+    "wifi-module",
+  ];
 
-    // search here for product - section
-    let part = document.getElementById('parts');
-    let search = document.getElementById('searchTxt');
-    search.addEventListener('input', function (e) {
-        let inputVal = search.value;
-        let html;
-        part.innerHTML = "";
-        prod.forEach(function (element) {
-            if (element.includes(inputVal)) {
-                html += `<div class="card">
+  // Generate product name string function
+  const getNameString = function (name) {
+    return name
+      .split("-")
+      .map((word) => {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      })
+      .join(" ");
+  };
+
+  //Display all products
+  let part = document.getElementById("parts");
+
+  const displayAllProducts = (prod) => {
+    let allProdHtml = "";
+
+    prod.forEach(function (element) {
+      allProdHtml += `<div class="card">
              <img src="assets/product-images/${element}.png" alt="" />
-             <p>${element}</p>
-           </div>`
-
-            }
-            part.innerHTML = html;
-        })
-        if (search.value == null) {
-            part.innerHTML = `<div class="card">  
-        <img src="assets/product-images/solar-charge-controller.png" alt="" />
-        <p>Solar Charge Controller</p>
-      </div>
-      <div class="card">
-        <img src="assets/product-images/lidar.png" alt="" />
-        <p>LIDAR</p>
-      </div>
-      <div class="card">
-        <img src="assets/product-images/gryo.png" alt="" />
-        <p>Gryo</p>
-      </div>
-      <div class="card">
-        <img src="assets/product-images/gryo.png" alt="" />
-        <p>Gryo</p>
-      </div>
-
-      <div class="card">
-        <img src="assets/product-images/lidar.png" alt="" />
-        <p>LIDAR</p>
-      </div>   `;
-        }
+             <p>${getNameString(element)}</p>
+           </div>`;
     });
 
-}
+    return allProdHtml;
+  };
+
+  part.innerHTML = displayAllProducts(prod);
+
+  // search here for product - section
+  let search = document.getElementById("searchTxt");
+  search.addEventListener("input", function (e) {
+    let inputVal = search.value.toLowerCase();
+    let html = "";
+    part.innerHTML = "";
+    prod.forEach(function (element) {
+      if (element.includes(inputVal)) {
+        console.log(element);
+        html += `<div class="card">
+             <img src="assets/product-images/${element}.png" alt="" />
+             <p>${getNameString(element)}</p>
+           </div>`;
+      }
+      part.innerHTML = html;
+    });
+    if (search.value == "") {
+      part.innerHTML = `${allProdHtml(prod)}`;
+    }
+  });
+};
